1dc5bb0a519bc891f45024432b923dc70dd37930
tiwa1154 <90009613+tiwa1154@users.noreply.github.com>
2022-04-05 10:03:15 -0400

    Delete readAndResizeImages.m

--- readAndResizeImages.m
+++
@@ -1,8 +0,0 @@
-function img = readAndResizeImages(filename)
-    % read image
-    im = imread(filename);
-    % resize image
-    img = imresize(im,[200 200]);
-    %Grey scale
-    %img = rgb2gray(img);
-end
